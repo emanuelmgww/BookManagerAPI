42045b91ad0c18aeef950a15a2a8b479b80585b6
emanuelmgww <emanuelmirandagaldino40197@gmail.com>
2025-03-18 19:12:41 -0300

    Utilização de um recurso chamado Required. Significa que se um desses campos forem nulos, a aplicação retornará a mensagem de erro e retorna um HTTP 400 (Bad Request).

--- Models/BookModel.cs
+++ Models/BookModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,15 @@ namespace BookManagerApi.Models
     public class BookModel
     {
         public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O Nome não pode estar vazio!")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O Genero não pode estar vazio!")]
         public string Genero { get; set; }
-        public string Autor { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "O Autor não pode estar vazio!")]
+        public string Autor { get; set; }
         public DateTime DataPublicacao { get; set; }
 
         public BookModel(string nome, string genero, string autor)
