7a934bb838d4dc6ba106193ad7a9aa954099464c
emanuelmgww <emanuelmirandagaldino40197@gmail.com>
2025-03-18 18:53:39 -0300

    Usando a string de conexão para iniciar o contexto do banco.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using BookManagerApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<BookDbContext>(options => 
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
